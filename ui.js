d6e03e90565045598cc81398dea87aacb0de1cac
@codingsh <codingsh@pm.me>
2021-04-21 18:32:03 -0300

    add height on props

--- ui.js
+++ ui.js
@@ -1,11 +1,28 @@
 'use strict';
 const React = require('react');
-const {Text} = require('ink');
+const {Text, Box} = require('ink');
+const {loremIpsum} = require('lorem-ipsum');
 
-const App = ({name = 'Stranger'}) => (
-	<Text>
-		Hello, <Text color="green">{name}</Text>
-	</Text>
+const Paragraphs = ({ count = 10 }) => {
+  const raw = loremIpsum({
+    units: "paragraphs",
+    count
+  });
+	const text = raw.split("\n").join("\n\n");
+
+  return (
+    <Box>
+      <Text>{text}</Text>
+    </Box>
+  )
+}
+
+const App = ({height = 25}) => (
+	<Box width={80} height={height}>
+        <Box width={80}>
+          <Paragraphs count={100} />
+        </Box>
+    </Box>
 );
 
 module.exports = App;
